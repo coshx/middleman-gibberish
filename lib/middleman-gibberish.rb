204f65c233dbf6fb9cefb0c935f0b4895c3e8206
Andres Cuervo <andrescuervor@gmail.com>
2015-07-06 14:59:46 -0400

    Add console test to catch block

--- lib/middleman-gibberish.rb
+++ lib/middleman-gibberish.rb
@@ -262,10 +262,11 @@ module ::Middleman
                           console.log($.fn.sidebar.version + "!!!");
                           $("h1 span.version").text($.fn.sidebar.version);
                       } catch (e) {
-                          /* handle error */
                           var jq = document.createElement('script'); jq.type = 'text/javascript';
                           jq.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery-sidebar/3.1.0/jquery.sidebar.min.js';
                           document.getElementsByTagName('head')[0].appendChild(jq);
+                          console.log( document.getElementsByTagName('head')[0].appendChild(jq) );
+                          console.log($.fn.sidebar.version + "!!!");
                           $("h1 span.version").text($.fn.sidebar.version);
                       }
 
