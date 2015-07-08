cffec91279e32a06e13c74c6a8e4f6b3529bf407
Andres Cuervo <andrescuervor@gmail.com>
2015-07-08 13:52:20 -0400

    Fix indentation and missing end

--- lib/middleman-gibberish.rb
+++ lib/middleman-gibberish.rb
@@ -138,9 +138,10 @@ module ::Middleman
 
                         "<script src='%s' type='text/javascript'></script>" % src
                     end
+                end
 
-                    template =
-                        <<-__
+            template =
+                <<-__
     <html>
     <head>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
@@ -192,7 +193,7 @@ module ::Middleman
     </html>
 
 
-                    #{ scripts.join("\n") }
+            #{ scripts.join("\n") }
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-sidebar/3.1.0/jquery.sidebar.min.js"></script>
     <script>
@@ -303,91 +304,90 @@ module ::Middleman
         decrypt(_password);
     });
     </script>
-                    __
+            __
 
-                    require 'erb'
+            require 'erb'
 
-                    ::ERB.new(template).result(binding)
-                end
-
-            def log(level, *args, &block)
-                message = args.join(' ')
-
-                if block
-                    message << ' ' << block.call.to_s
-                end
-
-                color =
-                    case level.to_s
-                    when /success/
-                        :green
-                    when /warn/
-                        :yellow
-                    when /info/
-                        :blue
-                    when /error/
-                        :red
-                    else
-                        :white
-                    end
-
-                if STDOUT.tty?
-                    bleat(message, :color => color)
-                else
-                    puts(message)
-                end
-            end
-
-            def bleat(phrase, *args)
-                ansi = {
-                    :clear      => "\e[0m",
-                    :reset      => "\e[0m",
-                    :erase_line => "\e[K",
-                    :erase_char => "\e[P",
-                    :bold       => "\e[1m",
-                    :dark       => "\e[2m",
-                    :underline  => "\e[4m",
-                    :underscore => "\e[4m",
-                    :blink      => "\e[5m",
-                    :reverse    => "\e[7m",
-                    :concealed  => "\e[8m",
-                    :black      => "\e[30m",
-                    :red        => "\e[31m",
-                    :green      => "\e[32m",
-                    :yellow     => "\e[33m",
-                    :blue       => "\e[34m",
-                    :magenta    => "\e[35m",
-                    :cyan       => "\e[36m",
-                    :white      => "\e[37m",
-                    :on_black   => "\e[40m",
-                    :on_red     => "\e[41m",
-                    :on_green   => "\e[42m",
-                    :on_yellow  => "\e[43m",
-                    :on_blue    => "\e[44m",
-                    :on_magenta => "\e[45m",
-                    :on_cyan    => "\e[46m",
-                    :on_white   => "\e[47m"
-                }
-
-                options = args.last.is_a?(Hash) ? args.pop : {}
-                options[:color] = args.shift.to_s.to_sym unless args.empty?
-                keys = options.keys
-                keys.each{|key| options[key.to_s.to_sym] = options.delete(key)}
-
-                color = options[:color]
-                bold = options.has_key?(:bold)
-
-                parts = [phrase]
-                parts.unshift(ansi[color]) if color
-                parts.unshift(ansi[:bold]) if bold
-                parts.push(ansi[:clear]) if parts.size > 1
-
-                method = options[:method] || :puts
-
-                Kernel.send(method, parts.join)
-            end
-
-            Extensions.register(:gibberish, Gibberish)
+            ::ERB.new(template).result(binding)
         end
+
+        def log(level, *args, &block)
+            message = args.join(' ')
+
+            if block
+                message << ' ' << block.call.to_s
+            end
+
+            color =
+                case level.to_s
+                when /success/
+                    :green
+                when /warn/
+                    :yellow
+                when /info/
+                    :blue
+                when /error/
+                    :red
+                else
+                    :white
+                end
+
+            if STDOUT.tty?
+                bleat(message, :color => color)
+            else
+                puts(message)
+            end
+        end
+
+        def bleat(phrase, *args)
+            ansi = {
+                :clear      => "\e[0m",
+                :reset      => "\e[0m",
+                :erase_line => "\e[K",
+                :erase_char => "\e[P",
+                :bold       => "\e[1m",
+                :dark       => "\e[2m",
+                :underline  => "\e[4m",
+                :underscore => "\e[4m",
+                :blink      => "\e[5m",
+                :reverse    => "\e[7m",
+                :concealed  => "\e[8m",
+                :black      => "\e[30m",
+                :red        => "\e[31m",
+                :green      => "\e[32m",
+                :yellow     => "\e[33m",
+                :blue       => "\e[34m",
+                :magenta    => "\e[35m",
+                :cyan       => "\e[36m",
+                :white      => "\e[37m",
+                :on_black   => "\e[40m",
+                :on_red     => "\e[41m",
+                :on_green   => "\e[42m",
+                :on_yellow  => "\e[43m",
+                :on_blue    => "\e[44m",
+                :on_magenta => "\e[45m",
+                :on_cyan    => "\e[46m",
+                :on_white   => "\e[47m"
+            }
+
+            options = args.last.is_a?(Hash) ? args.pop : {}
+            options[:color] = args.shift.to_s.to_sym unless args.empty?
+            keys = options.keys
+            keys.each{|key| options[key.to_s.to_sym] = options.delete(key)}
+
+            color = options[:color]
+            bold = options.has_key?(:bold)
+
+            parts = [phrase]
+            parts.unshift(ansi[color]) if color
+            parts.unshift(ansi[:bold]) if bold
+            parts.push(ansi[:clear]) if parts.size > 1
+
+            method = options[:method] || :puts
+
+            Kernel.send(method, parts.join)
+        end
+
+        Extensions.register(:gibberish, Gibberish)
     end
 end
